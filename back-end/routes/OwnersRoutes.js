4f107cf630befb024010d62839e8c3378184f8c6
massimopallera <pallera.massimo@gmail.com>
2025-01-09 17:23:36 +0100

    changed file names of owners

--- back-end/routes/OwnersRoutes.js
+++ back-end/routes/OwnersRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import controller from "../controllers/OwnersController.js"
+import controller from "../controllers/ownersController.js"
 const router = express.Router();
 
 //all owners
