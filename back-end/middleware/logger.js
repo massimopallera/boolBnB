233c4224008ba306cfa17ae23d4f801a29a16737
massimopallera <pallera.massimo@gmail.com>
2025-01-09 09:47:22 +0100

    updated logger

--- back-end/middleware/logger.js
+++ back-end/middleware/logger.js
@@ -1,7 +1,32 @@
+import chalk from "chalk";
+
 const logger = (req,res,next) => {
 
-    console.log('logging');
+    const time = new Date().toString()
+
+
+    const method = req.method
+    let methodColor;
+
+    if(method === "GET") methodColor = chalk.green
+    if(method === "POST") methodColor = chalk.bgYellow.black
+    if(method === "PUT") methodColor = chalk.yellow
+    if(method === "DELETE") methodColor = chalk.red
+
+    // const path = req.baseUrl !== '' ? '/' : req.baseUrl //doesn't find path
+
+    res.on('finish',()=>{
+        const status = res.statusCode
+        let statusColor;
+        if (status === 200 || status === 201) statusColor = chalk.green
+        else if (status === 404) statusColor = chalk.red
+
+
+        console.log(`Method: ${methodColor(method)} | Time: [${chalk.yellow(time)}] | Status: ${statusColor(status)}`)
+    })
     
+    
+
 
     next()
 }
