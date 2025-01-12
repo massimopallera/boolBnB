815a6092d98418f1089d50d36470de8816818ea2
massimopallera <pallera.massimo@gmail.com>
2025-01-12 11:27:37 +0100

    refactoring for sending emails

--- back-end/server.js
+++ back-end/server.js
@@ -1,16 +1,22 @@
 // ⏬ imports
 import express from "express"
+import dotenv from 'dotenv'
 import apartmentsRouter from "./routes/apartmentsRoutes.js"
 import reviewsRouter from "./routes/reviewsRoutes.js"
 import ownersRouter from "./routes/ownersRoutes.js"
 import logger from "./middleware/logger.js"
 import loginRouter from "./routes/loginRoutes.js"
+import infoRouter from "./routes/infoRoutes.js"
 import handlers from "./middleware/handlers.js"
-// import sendEmail from "./dev-mail-api/testMail.js"
+
+import cors from 'cors'
 
 const server = express()
 
+const tryRouter = express.Router()
+
 server.use(express.json())
+server.use(cors())
 
 const HOST = process.env.HOST || "http://localhost"
 const PORT = process.env.PORT || 3000
@@ -35,29 +41,24 @@ server.use('/reviews', reviewsRouter)
 server.use('/owner', ownersRouter)
 server.use('/login', loginRouter)
 
-import sgMail from '@sendgrid/mail'
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-const msg = {
+
+/* const msg = {
   to: 'test@example.com', // Change to your recipient
   from: process.env.EMAIL_SENDGRID, // Change to your verified sender
   subject: 'Sending with SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
 }
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email sent')
-  })
-  .catch((error) => {
-    console.error(error)
-  })
+ */
+
+// console.log(dotenv.config().parsed.SENDGRID_API_KEY);
 
 
 
 
-// server.use('/testAPImail', sendEmail)
+
+server.use('/testAPImail', infoRouter)
 
 // 🤝 handler
 server.use(handlers.NotFound)
