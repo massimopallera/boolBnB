1eadad5374f49d58ca317c87fa8c973a5a7e8eb4
giovanni-lumini <giovanni.lumini28@gmail.com>
2025-01-13 16:32:56 +0100

    DefaultLayout+Routes

---
+++ front-end/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+
+    return (
+        <>
+            <footer className="">
+                <div className="">&copy; {new Date().getFullYear()} BoolBnb</div>
+            </footer>
+        </>
+    )
+}
---
+++ front-end/components/Header.jsx
@@ -0,0 +1,12 @@
+/* import { Link } from "react-router-dom" */
+
+export default function Header() {
+
+    return (
+        <>
+            <header className="">
+                <h1>BoolBnb</h1>
+            </header>
+        </>
+    )
+}
