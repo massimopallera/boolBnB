017ff95f177a655c275daa2138f691f42135ebc6
MicheleFumi <michele.fumi96@gmail.com>
2025-01-14 15:13:07 +0100

    code cleanup

--- front-end/components/apartment/ReviewsSection.jsx
+++ front-end/components/apartment/ReviewsSection.jsx
@@ -1,30 +1,29 @@
 import { useEffect, useState } from "react";
 import dayjs from 'dayjs';
-import { useGlobalContext } from "../../context/GlobalContext";
 
 
-export default function ReviewsSection({id}){
+
+export default function ReviewsSection({ id }) {
 
     const [reviews, setReviews] = useState([])
-    
-    const reviewsApi = import.meta.env.VITE_EXPRESS_SERVER + "/reviews/"+ id
 
-    // const date = Date.now()
-    // const dayjsObj = dayjs(date)
+    const reviewsApi = import.meta.env.VITE_EXPRESS_SERVER + "/reviews/" + id
 
-   useEffect( () => {
-    fetch(reviewsApi)
-    .then(resp => resp.json())
-    .then(data => {setReviews(data.data)})
-    .catch(err => console.error(err))
-}, [])
-  
-   
-    return(
+
+
+    useEffect(() => {
+        fetch(reviewsApi)
+            .then(resp => resp.json())
+            .then(data => { setReviews(data.data) })
+            .catch(err => console.error(err))
+    }, [])
+
+
+    return (
         <>
             <div>
                 <h3>Recensioni</h3>
-                {reviews.map((review,index) => (
+                {reviews.map((review, index) => (
                     <div key={index}>
                         <div> <strong>name</strong> : {review.name}</div>
                         <div> <strong>text</strong> : {review.text}</div>
