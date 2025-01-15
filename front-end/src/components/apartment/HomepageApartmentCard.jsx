421bd135c00b30e9c5e2fe883c743874ff5379d8
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2025-01-15 10:27:10 +0100

    homepage card style update

--- front-end/src/components/apartment/HomepageApartmentCard.jsx
+++ front-end/src/components/apartment/HomepageApartmentCard.jsx
@@ -8,16 +8,17 @@ export default function HomepageApartmentCard({ apartment }) {
         <>
 
             <div className="col">
-                <Link to={`/apartments/${apartment.id}`} className="overviewCard card bg-light bg-gradient shadow-sm d-flex flex-column p-3">
+                <Link to={`/apartments/${apartment.id}`} className="overviewCard card bg-light bg-gradient shadow-sm d-flex flex-column ">
                     <span> <img
                         src={apartment.apartment_images || "/placeholder.png"}
                         alt="Apartment"
                         className="img-fluid rounded shadow-sm"
                         style={{ maxHeight: '150px', objectFit: 'cover' }}
                     /></span>
-                    <div className="card-body bg-white border mt-4">
-                        <strong>Descrizione:</strong><p>  {apartment.description}</p>
-                        <span> <strong>Indirizzo:</strong> {apartment.address}</span>
+                    <div className="card-body mt-2">
+                        <div className="mb-2"><strong>Descrizione:</strong>  {apartment.description}</div>
+
+                        <div> <strong>Indirizzo:</strong> {apartment.address}</div>
                     </div>
                     <div className="p-2">
                         <i className="bi bi-heart-fill mt-3 text-danger">  {apartment.hearts_counter}</i>
