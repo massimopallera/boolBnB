7c852f67256ff13f11c81a797377b633c381c3b2
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2025-01-15 13:00:22 +0100

    fix detailApartmentCard style

--- front-end/src/components/apartment/DetailApartmentCard.jsx
+++ front-end/src/components/apartment/DetailApartmentCard.jsx
@@ -6,63 +6,65 @@ export default function DetailApartmentCard({ id }) {
 
     useEffect(() => {
         fetch(`http://127.0.0.1:3000/apartments/${id}`)
-           .then(resp => resp.json())
-           .then(data => { setApartment(data.data[0]); setLikesCounter(data.data[0].hearts_counter);})
-           .catch(err => console.log(err))
-    },[])
+            .then(resp => resp.json())
+            .then(data => { setApartment(data.data[0]); setLikesCounter(data.data[0].hearts_counter); })
+            .catch(err => console.log(err))
+    }, [])
 
 
-    function handleClick(){
+    function handleClick() {
         setLikesCounter(likesCounter + 1)  // increment likes counter
         // send like to server
         fetch(`http://localhost:3000/apartments/${id}`, {
             method: 'PUT',
-            body: JSON.stringify({hearts_counter: likesCounter}),
-            headers:{ "Content-Type": "application/json" }
+            body: JSON.stringify({ hearts_counter: likesCounter }),
+            headers: { "Content-Type": "application/json" }
         })
-           .then(resp => resp.json())
-           .then(data => console.log('fatto')
-           )
-           .catch(err => console.log(err))
+            .then(resp => resp.json())
+            .then(data => console.log('fatto')
+            )
+            .catch(err => console.log(err))
     }
 
     return (
         <>
-        { apartment ? (
+            {apartment ? (
 
-            <div className="container-sm m-auto ">
-                <div className="card bg-light bg-gradientd-flex flex-column p-3">
-                    <div className="d-flex justify-content-center mb-3">
-                        <img
-                            src={apartment.apartment_images || "/placeholder.png"}
-                            alt="apartment"
-                            className="img-fluid rounded shadow-sm"
-                            style={{ maxHeight: '500px', objectFit: 'cover' }}
-                        />
-                    </div>
-                    <div className="card mb-4">
-                        <div className="card-body">
-                            <ul className="list-unstyled">
-                                <li><strong>Descrizione:</strong> {apartment.description}</li>
-                                <li><strong>Stanze:</strong> {apartment.rooms}</li>
-                                <li><strong>Letti:</strong> {apartment.beds}</li>
-                                <li><strong>Bagni:</strong> {apartment.toilets}</li>
-                                <li><strong>Mq:</strong> {apartment.sq_meters}</li>
-                                <li><strong>Indirizzo:</strong> {apartment.address}</li>
-                                <li><strong>Email:</strong> {apartment.email}</li>
-                                <li><strong>Servizi aggiuntivi:</strong> {apartment.added_services}</li>
-                            </ul>
+                <div className="container m-auto ">
+                    <div className="bg-lightflex-row bg-gradientd-flex row">
+                        <div className=" d-flex  row justify-content-center flex-wrap border shadow rounded p-4 bg-light bg-gradient">
+                            <div className="col-sm-12 col-lg-8 p-0 m-0">
+                                <img
+                                    src={apartment.apartment_images || "/placeholder.png"}
+                                    alt="apartment"
+                                    className="img-fluid rounded"
+                                />
+                            </div>
+
+                            <div className="col-sm-12 col-lg-4">
+
+                                <div className=" ">
+                                    <ul className="list-unstyled p-4">
+                                        <li><strong>Descrizione:</strong> {apartment.description}</li>
+                                        <li><strong>Stanze:</strong> {apartment.rooms}</li>
+                                        <li><strong>Letti:</strong> {apartment.beds}</li>
+                                        <li><strong>Bagni:</strong> {apartment.toilets}</li>
+                                        <li><strong>Mq:</strong> {apartment.sq_meters}</li>
+                                        <li><strong>Indirizzo:</strong> {apartment.address}</li>
+                                        <li><strong>Email:</strong> {apartment.email}</li>
+                                        <li><strong>Servizi aggiuntivi:</strong> {apartment.added_services}</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="d-flex align-items-center fs-4 mx-3 mt-3">
+                                <i onClick={() => handleClick()} className="bi bi-heart-fill text-danger"> </i>
+                                <span>{likesCounter}</span>
+                            </div>
                         </div>
                     </div>
-                        <div className="d-flex align-items-center fs-4 gap-2">
-                            <i onClick={() => handleClick()} className="bi bi-heart-fill text-danger"> </i>
-                            <span>{likesCounter}</span>   
-                        </div>
-                       
                 </div>
-            </div>
             ) : null
-    }
+            }
         </>
     )
 }
