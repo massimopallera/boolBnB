74b797672c276139c879799ec9030a930af081cb
MicheleFumi <michele.fumi96@gmail.com>
2025-01-18 13:04:44 +0100

    add searchbar

---
+++ front-end/src/components/Homepage/SearchFields.jsx
@@ -0,0 +1,100 @@
+import { useState, useEffect } from "react";
+import { useGlobalContext } from "../../context/GlobalContext";
+
+export default function SearchField({ setFilteredApartments }) {
+    const { apartments } = useGlobalContext();
+    const [searchFields, setSearchFields] = useState({
+        address: "",
+        beds: "",
+        rooms: "",
+    });
+
+    function onChange(e) {
+        const { id, value } = e.target;
+        setSearchFields((prev) => ({
+            ...prev,
+            [id]: value,
+        }));
+    }
+
+
+    useEffect(() => {
+        const filtered = apartments.filter((apartment) => {
+
+            const matchesLocation = searchFields.address
+                ? apartment.address.toLowerCase().includes(searchFields.address.toLowerCase())
+                : true;
+
+            const matchesBeds = searchFields.beds
+                ? apartment.beds === parseInt(searchFields.beds)
+                : true;
+            const matchesRooms = searchFields.rooms
+                ? apartment.rooms === parseInt(searchFields.rooms)
+                : true;
+
+
+            return matchesLocation && matchesBeds && matchesRooms;
+        });
+
+        setFilteredApartments(filtered);
+    }, [searchFields, apartments, setFilteredApartments]);
+
+    return (
+        <div className="mb-3">
+            <div className="container mt-3">
+                <div className="row g-3">
+                    {/*  Searchbar: Località */}
+                    <div className="col-lg-4 col-md-6 col-sm-12">
+                        <div className="form-group">
+                            <label htmlFor="address" className="form-label">
+                                Località
+                            </label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="address"
+                                placeholder="Inserisci la località"
+                                onChange={onChange}
+                                value={searchFields.address}
+                            />
+                        </div>
+                    </div>
+
+                    {/*  Searchbar: Numero minimo di posti letto */}
+                    <div className="col-lg-4 col-md-6 col-sm-12">
+                        <div className="form-group">
+                            <label htmlFor="beds" className="form-label">
+                                Posti letto
+                            </label>
+                            <input
+                                type="number"
+                                className="form-control"
+                                id="beds"
+                                placeholder="Min. posti letto"
+                                onChange={onChange}
+                                value={searchFields.beds}
+                            />
+                        </div>
+                    </div>
+
+                    {/*  Searchbar: Numero minimo di stanze */}
+                    <div className="col-lg-4 col-md-6 col-sm-12">
+                        <div className="form-group">
+                            <label htmlFor="rooms" className="form-label">
+                                Stanze
+                            </label>
+                            <input
+                                type="number"
+                                className="form-control"
+                                id="rooms"
+                                placeholder="Min. stanze"
+                                onChange={onChange}
+                                value={searchFields.rooms}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- front-end/src/pages/Homepage.jsx
+++ front-end/src/pages/Homepage.jsx
@@ -1,23 +1,30 @@
-import { useGlobalContext } from '../context/GlobalContext'
-import HomepageCard from '../components/Homepage/HomepageCard'
+
+import { useState } from "react";
+import { useGlobalContext } from "../context/GlobalContext";
+import HomepageCard from "../components/Homepage/HomepageCard";
+import SearchField from "../components/Homepage/SearchFields";
 
 export default function HomePage() {
-
-    const { apartments } = useGlobalContext()
+    const { apartments } = useGlobalContext();
+    const [filteredApartments, setFilteredApartments] = useState(apartments);
 
     return (
-        <>
-            <div className="container" style={{ maxWidth: "1800px" }}>
-                <h2 className=' pb-4'>STRUTTURE: </h2>
-                <div className='row row-cols-sm-12 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 align-items-stretch justify-content-center g-5'>
-
-                    {apartments.map(apartment => (
-                        <HomepageCard apartment={apartment} key={apartment.id} />
-                    ))
-                    }
-                </div>
+        <div className="container" style={{ maxWidth: "1800px" }}>
+            <SearchField setFilteredApartments={setFilteredApartments} />
+            <h2 className="pb-4">STRUTTURE: </h2>
+            {!filteredApartments ? (<div className="row row-cols-sm-12 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 align-items-stretch justify-content-center g-5">
+                {filteredApartments.map((apartment) => (
+                    <HomepageCard apartment={apartment} key={apartment.id} />
+                ))}
             </div>
-        </>
+            ) : (<>
+                <div>
+                    <h5>Spiacente, non ho trovato nulla con i requisiti richiesti.</h5>
+                </div>
 
-    )
+
+            </>)}
+        </div>
+
+    );
 }
